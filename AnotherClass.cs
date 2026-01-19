e71b099e2ea0551f97d39beb91bb768204664176
joshmccauley7 <joshmccauley7@gmail.com>
2026-01-19 15:51:47 -0700

    added another class

---
+++ AnotherClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitFun
+{
+    internal class AnotherClass
+    {
+        public void DoSomething()
+        { }
+    }
+}
